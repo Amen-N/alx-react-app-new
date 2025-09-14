f534b300caf5b840c2fbfe019caf81d8c95265bd
Amen N. <amen4nigussie@gmail.com>
2025-09-15 00:09:16 +0300

    Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,14 +1,14 @@
 function Header() {
   return (
     <header
-      style={{
-        backgroundColor: 'navy',
-        color: 'white',
-        textAlign: 'center',
-        padding: '20px'
-      }}
+      style{
+        backgroundColor: navy;
+        color: white;
+        textAlign: center;
+        padding: 20px;
+      }
     >
-      <h1>My Favorite Cities</h1>
+      <h1> My Favorite Cities </h1>
     </header>
   );
 }
